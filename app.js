26304dd97fb69ede2708714b5363af19f13834da
Yash <yashvdkd@gmail.com>
2022-10-19 19:29:42 +0530

    feat & refactor: all done

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
 var btnTranslate = document.querySelector("#btnTranslate");
+var inputDiv = document.querySelector("#input");
+var outputDiv = document.querySelector("#output");
 
 // btnTranslate.addEventListener("click", function clickHandler() {
 //     console.log("Clicked");
 // });
 
-var inputDiv = document.querySelector("#input");
-var outputDiv = document.querySelector("#output");
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
 
-btnTranslate.addEventListener("click", clickHandler);
+function getTranslationURL(input) {
+    return serverURL + "?text=" + input;
+}
+
+function errorHandler(error) {
+    console.log("error occured" + error);
+    alert("Something went wrong with the server. Try again after some time.")
+}
 
 function clickHandler () {
-    outputDiv.innerText = "hakuna" + inputDiv.value + "matatatatatatatata";
-    console.log("Clicked");
+    var inputText = inputDiv.value;
+    fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json => {var translatedText = json.contents.translated
+        outputDiv.innerText = translatedText;
+    })
+    .catch(errorHandler);
 }
+
+btnTranslate.addEventListener("click", clickHandler);
